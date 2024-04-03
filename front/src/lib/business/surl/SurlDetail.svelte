adb8f480be7c2dbe96a49f0dacbb4d270888d8e7
jhs512 <jangka512@gmail.com>
2024-04-03 09:50:00 +0900

    work

---
+++ front/src/lib/business/surl/SurlDetail.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { prettyDateTime } from '$lib/utils/index';
+	import type { components } from '$lib/backend/apiV1/schema';
+
+	const { surl }: { surl: components['schemas']['SurlDto'] } = $props();
+</script>
+
+<div class="flex flex-grow items-center justify-center">
+	<div class="mt-4 w-full px-2">
+		<div class="card bg-base-100 shadow">
+			<div class="card-body">
+				<div class="detail grid grid-cols-[repeat(auto-fit,minmax(100px,1fr))] gap-3">
+					<div class="form-control">
+						<!-- svelte-ignore a11y-label-has-associated-control -->
+						<label class="label">
+							<span class="label-text">번호</span>
+						</label>
+						<div>{surl.id}</div>
+					</div>
+
+					<div class="form-control">
+						<!-- svelte-ignore a11y-label-has-associated-control -->
+						<label class="label">
+							<span class="label-text">생성일</span>
+						</label>
+						<div>{prettyDateTime(surl.createDate)}</div>
+					</div>
+
+					<div class="form-control">
+						<!-- svelte-ignore a11y-label-has-associated-control -->
+						<label class="label">
+							<span class="label-text">작성자</span>
+						</label>
+						<div>{surl.authorId}</div>
+					</div>
+
+					<div class="form-control col-span-full">
+						<!-- svelte-ignore a11y-label-has-associated-control -->
+						<label class="label">
+							<span class="label-text">제목</span>
+						</label>
+						<div>
+							{surl.title}
+						</div>
+					</div>
+
+					<div class="form-control col-span-full">
+						<!-- svelte-ignore a11y-label-has-associated-control -->
+						<label class="label">
+							<span class="label-text">내용</span>
+						</label>
+						<div>
+							{surl.body}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- front/src/routes/surl/[id]/+page.svelte
+++ front/src/routes/surl/[id]/+page.svelte
@@ -1,1 +1,19 @@
-하하
+<script lang="ts">
+	import SurlDetail from '$lib/business/surl/SurlDetail.svelte';
+	//import SurlCommentWriteAndList from '$lib/business/surlComment/SurlCommentWriteAndList.svelte';
+
+	const { data } = $props();
+	const { surl } = data;
+</script>
+
+<svelte:head>
+	<title>{surl.title}</title>
+	<meta name="description" content={surl.body} />
+</svelte:head>
+
+<div class="flex flex-grow items-center justify-center">
+	<div class="mt-4 w-full px-2">
+		<SurlDetail {surl} />
+		<!-- <SurlCommentWriteAndList {surl} /> -->
+	</div>
+</div>
